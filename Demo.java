e7d0d2a6313ffb2a568cb1b51d7ceb95be5db961
chanthoeurntuon20 <54494449+chanthoeurntuon20@users.noreply.github.com>
2019-12-14 10:28:32 +0700

    Testing
    Demo

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package testDemo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Testing person1 = new Testing(24, "bopha", "Banteaymeanchey");
+		Testing person2 = new Testing(20, "chompa", "Kampong Cham");
+		
+		System.out.println(person1);
+		System.out.println(person2);
+	}
+
+}
